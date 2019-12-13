c76d0065ef24fb38dcb3b9c566890b2c0d3834b7
NguyenThaiQuyen <thai.quyen012@gmail.com>
2019-12-13 22:34:06 +0700

    Fix logout

--- src/packages/admin/LogoutServlet.java
+++ src/packages/admin/LogoutServlet.java
@@ -5,24 +5,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet("/AdminServlet")
-public class AdminServlet extends HttpServlet {
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        AdminBO adminBO = new AdminBO();
-        Admin admin = adminBO.getAdmin(username, password);
-        if (admin != null) {
-            request.getSession().setAttribute("admin", admin);
-            response.sendRedirect("DashBoardServlet");
-        } else {
-            response.sendRedirect("index.jsp");
-        }
+        doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().setAttribute("admin", null);
         response.sendRedirect("index.jsp");
     }
 }
